f8198cda4dd186ad9b4ba1e6d63ed1bf673a1304
Moupiya Sahoo <163728340+2300031445@users.noreply.github.com>
2025-09-26 08:27:04 +0530

    Update backend_url.ts

--- src/lib/backend_url.ts
+++ src/lib/backend_url.ts
@@ -1,4 +1,4 @@
 // Central backend URL for API requests
 // export const backend_url = "http://localhost:2028";
-export const backend_url = "http://localhost:9090";
+export const backend_url = "http://localhost:9090/api";
 
