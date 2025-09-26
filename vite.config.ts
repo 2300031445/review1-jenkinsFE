ee6800cc152e8de076a178e0dbadeee76bb69f82
Moupiya Sahoo <163728340+2300031445@users.noreply.github.com>
2025-09-26 08:47:20 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // base: '/onlinevoting/',
-  base: '/',
+  base: '/onlinevoting',
+  // base: '/',
   build: {
     outDir: "dist"
   },
